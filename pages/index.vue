a2213d517f2167d38a8759eee1b061ff6cfd6afc
Florent Girard <mrgirardflorent@gmail.com>
2022-10-18 15:18:05 +0200

    feat(static): add picture + fonts +vars

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,9 @@
 <template>
   <div>
-    <h1>{{ home.title }}</h1>
+    <h1>Hello</h1>
   </div>
 </template>
 
-<script lang="ts" setup>
-const home = reactive({
-  title: 'hello',
-})
-</script>
+<script lang="ts" setup></script>
 
-<style scoped></style>
+<style scoped lang="scss"></style>
